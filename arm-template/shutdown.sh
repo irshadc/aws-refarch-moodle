3b68e5c931e18f81236bf6aa8b586b9f3952568c
Irshad Chohan <irshad.comp@gmail.com>
2021-04-26 14:15:03 +0000

    Moodle ARM

---
+++ arm-template/shutdown.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#stopping bastion server
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name MoodleAppDemo-bastion-U6QKARE7BW6A-BastionAutoScalingGroup-16QYI95QQC6DH --min-size 0 --desired-capacity 0
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name MoodleAppDemo-efsfilesystem-ST8OZV0SFUAY-AutoScalingGroup-ULWXPH4U3BLB --min-size 0 --desired-capacity 0
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name MoodleAppDemo-webnocache-WMHCH2NN2GC0-WebAutoScalingGroup-PTNT8U2XICZK --min-size 0 --desired-capacity 0
+aws rds stop-db-cluster --db-cluster-identifier moodleappdemo-rds-1f4mpq56bwmr9-databasecluster-k3qk09xa71dn
